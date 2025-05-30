386d44896e84ecf7c34a558f37223310aecaaf14
LuisEduSouza <161759426+LuisEduSouza@users.noreply.github.com>
2025-05-30 20:53:43 -0300

    Add files via upload
    30/05 -> Treinando if, else, else if e operações matemáticas

--- ConsoleAppAula1/ConsoleAppAula1/Program.cs
+++ ConsoleAppAula1/ConsoleAppAula1/Program.cs
@@ -1,1 +1,94 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/* Menor ou Maior de idade
+int idade;
+//Console.WriteLine("Informe sua idade:");
+
+//idade = Convert.ToInt32(Console.ReadLine());
+
+//if (idade >= 18)
+//{
+//    Console.WriteLine($"Sua idade é {idade},você é maior de idade");
+//} else
+//{
+//    Console.WriteLine($"Sua idade é {idade},você é menor de idade");
+//}
+*/
+
+/** Maior ou menor que o salário mínimo
+const decimal salarioMinimo = 1518m;
+
+Console.WriteLine("Digite seu salário: R$ ");
+decimal salarioPessoal = decimal.Parse(Console.ReadLine());
+
+if (salarioPessoal >= salarioMinimo)
+{
+    Console.WriteLine($"Seu salário é maior que um salário mínimo. {salarioPessoal:C}");
+}
+else
+{
+    Console.WriteLine($"Você recebe um salário menor que o salário mínimo {salarioPessoal:C}");
+}
+**/
+
+float num1 = 0f;
+float num2 = 0f;
+string operacaoMatematica;
+
+do
+{
+    Console.WriteLine("Informe o primeiro valor:\n " +
+    "+ ou - ou * ou / ou s para sair");
+
+    operacaoMatematica = Console.ReadLine();
+
+    /** Está incompleto por enquanto
+    if(operacaoMatematica == "+" || operacaoMatematica == "-" || operacaoMatematica == "*" || operacaoMatematica == "/")
+    **/
+
+    Console.WriteLine("Informe o primeiro valor");
+    num1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe o segundo valor");
+    num2 = float.Parse(Console.ReadLine());
+
+    if (operacaoMatematica == "+")
+    {
+        //Soma
+        Console.WriteLine($"Soma: {num1 + num2}");
+        Console.ReadLine();
+    }
+    else if (operacaoMatematica == "-")
+    {
+        //Subtração
+        Console.WriteLine($"Subtração: {num1 - num2}");
+        Console.ReadLine();
+    }
+    else if (operacaoMatematica == "*")
+    {
+        //Multiplicação
+        Console.WriteLine($"Multiplicação: {num1 * num2}");
+        Console.ReadLine();
+    }
+    else if (operacaoMatematica == "/")
+    {
+        //Divisão
+        Console.WriteLine($"Divisão: {num1 / num2}");
+        Console.ReadLine();
+    }
+    else if (operacaoMatematica == "s")
+    {
+        //Saindo
+        Console.WriteLine("Desligando...");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Digite uma operação disponíveis");
+        Console.ReadLine();
+    }
+
+    Console.Clear();
+} while (operacaoMatematica != "s");
+
+
+
+
